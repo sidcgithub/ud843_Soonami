8994d9d882b26df835f36c2654d34857cf47f5bf
sidcoder <siddharthchordia@Siddharths-MacBook-Pro.local>
2018-08-29 18:43:52 +0400

    Added a handle for invalid http request

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** URL to query the USGS dataset for earthquake information */
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-12-01&minmagnitude=7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -164,6 +164,10 @@ public class MainActivity extends AppCompatActivity {
                 urlConnection.connect();
                 inputStream = urlConnection.getInputStream();
                 jsonResponse = readFromStream(inputStream);
+                if(!jsonResponse.contains("\"status\":200"))//should have used urlConnection.getresponsecode =200  .. Mistake
+                {
+                    jsonResponse = "";
+                }
             } catch (IOException e) {
                 // TODO: Handle the exception
             } finally {
@@ -202,6 +206,8 @@ public class MainActivity extends AppCompatActivity {
          */
         private Event extractFeatureFromJson(String earthquakeJSON) {
             try {
+                if(earthquakeJSON=="")
+                    return null;
                 JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
                 JSONArray featureArray = baseJsonResponse.getJSONArray("features");
 
